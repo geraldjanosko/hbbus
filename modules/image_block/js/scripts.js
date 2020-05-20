61f3a2e807421bbf6938a4f5d84631c6ddc785bb
Gerald Janosko <geraldjanosko@gmail.com>
2020-05-20 12:08:08 -0400

    Fix for dropdown

--- modules/image_block/js/scripts.js
+++ modules/image_block/js/scripts.js
@@ -1,6 +1,6 @@
 (function($){
-	var defaultURL = drupalSettings.path.baseUrl + drupalSettings.path.themeUrl + '/images/default.jpg';
-  $( ".image-block-img" ).error(function() {
+  var defaultURL = drupalSettings.path.baseUrl + drupalSettings.path.themeUrl + '/images/default.jpg';
+  $( ".image-block-img" ).on('error', function() {
     $(this).attr("src", defaultURL);
   })
 }(jQuery));
